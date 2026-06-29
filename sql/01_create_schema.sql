951dfb25d1dbb2f04cb94b225185969658e17b29
renegahino06 <76971607+renegahino06@users.noreply.github.com>
2026-06-29 08:09:22 -0600

    Create 01_create_schema.sql

---
+++ sql/01_create_schema.sql
@@ -0,0 +1,7 @@
+-- Crear base de datos o esquema
+-- Ajustar según el SGBD (PostgreSQL, MySQL, etc.)
+
+CREATE DATABASE gestion_aulas;
+
+-- En PostgreSQL, se podría usar:
+-- CREATE SCHEMA IF NOT EXISTS gestion_aulas;
